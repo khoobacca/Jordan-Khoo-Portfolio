e1eab4f69de00607d904afff2a66451206219929
Jordan Khoo <jordanwrkhoo@gmail.com>
2024-01-01 20:07:36 -0700

    more work

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ renderer.setSize(window.innerWidth,window.innerHeight);
 // renderer.setClearColor(0x000000, 0.0);
 
 // document.body.appendChild(renderer.domElement);
-camera.position.setZ(20);
+camera.position.setZ(15);
 
 // to render an object in 3d, we need a set of points to define the object, a material to colour the object,
 // and a mesh to combine the points and material into a polygon in x dimensions needed.
@@ -40,15 +40,16 @@ const torus = new THREE.Mesh(torus_geometry, torus_material);
 
 // stars:
 const star_geometry = new THREE.SphereGeometry(0.25);
-const white_star_material = new THREE.MeshStandardMaterial({color: 0xffffff, emissive: 0xffffeb})
-const blue_star_material = new THREE.MeshStandardMaterial({color: 0xffffff, emissive: 0x56fffc})
-const yellow_star_material = new THREE.MeshStandardMaterial({color: 0xffffff, emissive: 0xfa2a2a})
+const white_star_material = new THREE.MeshStandardMaterial({emissive: 0xffffeb})
+const blue_star_material = new THREE.MeshStandardMaterial({emissive: 0x56fffc})
+const yellow_star_material = new THREE.MeshStandardMaterial({emissive: 0xffe90f})
+const red_star_material = new THREE.MeshStandardMaterial({emissive: 0xfd1818})
 
 // planets:
 // const planet1Texture = new THREE.TextureLoader().load('venus_texture.jpg');
-const planet1Texture = new THREE.TextureLoader().load('venus_texture.jpg');
-const planet2Texture = new THREE.TextureLoader().load('mars_texture.jpg');
-const planet3Texture = new THREE.TextureLoader().load('neptune_texture.jpg');
+const planet1Texture = new THREE.TextureLoader().load('./assets/venus_texture.jpg');
+const planet2Texture = new THREE.TextureLoader().load('./assets/mars_texture.jpg');
+const planet3Texture = new THREE.TextureLoader().load('./assets/neptune_texture.jpg');
 
 const planet1 = new THREE.Mesh(
     new THREE.SphereGeometry(3,32,32),
@@ -74,7 +75,7 @@ const planet3 = new THREE.Mesh(
 // light sources:
 const pointLightSource = new THREE.PointLight(0xffffff);
 const ambientLightSource = new THREE.AmbientLight(0xffffff);
-pointLightSource.position.set(5,5,5);
+pointLightSource.position.set(30,10,5);
 
 // help visualize sources/objects:
 const lightHelper = new THREE.PointLightHelper(pointLightSource);
@@ -83,10 +84,8 @@ const gridHelper = new THREE.GridHelper(200,50);
 // user controls
 const controls = new OrbitControls(camera, renderer.domElement);
 
-function moonOrbit(){};
-
-function ObjRotate(planet, x, y, z){
-    planet.rotateOnAxis(new THREE.Vector3(x,y,z), 0.0025);
+function rotatePlanet(planet, speed){
+    planet.rotateOnAxis(new THREE.Vector3(0,1,0), speed);
 };
 
 // rendering a scene:
@@ -94,18 +93,31 @@ function animate(){
     // renders every screen refresh (60Hz etc), and pauses when user is not on browser tab
     requestAnimationFrame(animate);
     
-    ObjRotate(planet1, );
-    ObjRotate(planet2, );
-    ObjRotate(planet3, );
+    rotatePlanet(planet1, 0.005);
+    rotatePlanet(planet2, 0.005);
+    rotatePlanet(planet3, 0.005);
 
     controls.update();
 
     renderer.render(scene, camera);
 }
 
+// camera movement
+// function moveCamera(){
+//     const loc = document.body.getBoundingClientRect().top;
+//     console.log(loc);
+//     camera.position.z = 15 + loc * -0.04;
+// }
+// document.body.onscroll = moveCamera
+
+// mouse event
+// document.addEventListener('mousedown', onDocumentMouseDown, false);
+// document.addEventListener('mousemove', onDocumentMouseMove, false);
+
+// star generation
 function addWhiteStar(){
     const star = new THREE.Mesh(star_geometry, white_star_material);
-    const[x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(300));
+    const[x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(400));
 
     star.position.set(x,y,z);
     scene.add(star)
@@ -113,7 +125,7 @@ function addWhiteStar(){
 
 function addBlueStar(){
     const star = new THREE.Mesh(star_geometry, blue_star_material);
-    const[x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(300));
+    const[x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(400));
 
     star.position.set(x,y,z);
     scene.add(star)
@@ -121,36 +133,44 @@ function addBlueStar(){
 
 function addYellowStar(){
     const star = new THREE.Mesh(star_geometry, yellow_star_material);
-    const[x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(300));
+    const[x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(400));
+
+    star.position.set(x,y,z);
+    scene.add(star)
+}
+
+function addRedStar(){
+    const star = new THREE.Mesh(star_geometry, red_star_material);
+    const[x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(400));
 
     star.position.set(x,y,z);
     scene.add(star)
 }
 
 // adding objects to scene
-scene.add(pointLightSource, ambientLightSource);
-scene.add(lightHelper, gridHelper);
+scene.add(pointLightSource);
+scene.add(gridHelper, lightHelper);
 
 // planet rotations
 scene.add(planet1, planet2, planet3);
 planet1.position.set(3, 2, 0);
-planet2.position.set(-4,-4, 30);
-planet3.position.set(5, 3, 60);
+planet2.position.set(-8,-4, 25);
+planet3.position.set(5, 2, 60);
 
 planet1.rotateZ(-0.25);
-
 planet2.rotateY(0.25);
-planet2.rotateX(0.0125);
-
+planet2.rotateX(0.0125,0.25,0);
 planet3.rotateZ(-0.25);
 planet3.rotateX(-0.0125);
 
 // stars
-Array(300).fill().forEach(addWhiteStar);
-Array(100).fill().forEach(addBlueStar);
-Array(100).fill().forEach(addBlueStar);
+Array(600).fill().forEach(addWhiteStar);
+Array(200).fill().forEach(addBlueStar);
+Array(200).fill().forEach(addYellowStar);
+Array(200).fill().forEach(addRedStar);
 animate();
 
 // background
-// const spaceBG = new THREE.TextureLoader().load('./assets/Starfield.png');
-scene.background.colour(0x07074e);
+const spaceBG = new THREE.TextureLoader().load('./assets/space_bg.png');
+scene.background = spaceBG;
+scene.background.emissive(0x07074e);
